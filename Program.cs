d052a9d70c4d04bac01f603280a194e0782c2bc3
javohir <javohirbaxridinov77@gmail.com>
2023-02-28 22:59:38 +0700

    sdfgnfdgnergrfsdgtghmmhuj

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Console.WriteLine(" введите число ");
+
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(" ввидите вторе число ");
+
+// int numberA = Convert.ToInt32(Console.ReadLine());
+
+// int max = number; 
+
+
+// if (numberA > max )
+// {
+//     max = numberA; 
+//  Console.WriteLine(max); 
+ 
+// }
+//  else
+//  {
+//    Console.WriteLine (number + " макцималное число ");
+//  }
+
+//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22]
+
+//  Console.WriteLine(" Введите число ");
+
+//  int numberA = Convert.ToInt32(Console.ReadLine());
+
+//   Console.WriteLine(" ввидите вторе число "); 
+
+//  int numberB = Convert.ToInt32(Console.ReadLine());
+ 
+// Console.WriteLine(" ввидите третое число ");
+
+//  int numberC = Convert.ToInt32(Console.ReadLine());
+
+//   int max = numberA; 
+
+//  if (numberB > max) 
+
+//  {
+//      max = numberB; 
+
+//  }
+
+// if (numberC > max) 
+ 
+//  {
+//      max = numberC;
+//  }
+  
+//    Console.WriteLine (max + " макцималное число ");
+
+
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine(" Введите число ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if  (number % 2 == 0) 
+// {
+// Console.WriteLine ("да чотное ");
+// }
+// else
+// {
+//     Console.WriteLine ("нет ");
+// }
+
+//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+ Console.WriteLine(" Введите число ");
+
+  int number = Convert.ToInt32(Console.ReadLine());
+
+int num = 0;
+
+while (num <= number ) 
+
+{
+     Console.WriteLine ( num );
+
+     num+= 2;
+}
+
+
